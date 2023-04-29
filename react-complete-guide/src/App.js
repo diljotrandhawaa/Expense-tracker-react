24b78d4d71b3f3ae65fb54d42cbb33789c6665be
diljotrandhawaa <learning287@gmail.com>
2023-04-29 21:22:21 +0530

    second change complete

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi, I'm Diljot</h1>
+        <h1>Hi, I'm Diljot Randhawa and I'm gonna learn React</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
