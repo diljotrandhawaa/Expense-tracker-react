38f4b5ecf6375dc08dd517bcb3439f8798d4b28f
diljotrandhawaa <learning287@gmail.com>
2023-05-01 12:41:25 +0530

    useState added to change expense title just checking

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+
+    const [title, setTitle] = useState(props.title);
+
+    function clickHandler () {
+        setTitle('Updated!');
+        console.log('Clicked!!!!!!!!!!!!!!!');
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandler}>Change title</button>
             </div>
         </Card>
     )
